b116c8641dd85a05201ee2563b751efab85dc476
The Server Boutique <hello.theserverboutique@gmail.com>
2026-08-02 12:58:07 +0100

    Create main export file

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/**
+ * The Server Boutique - Discord Utils
+ * Main export file
+ */
+
+module.exports = {
+    // Embeds
+    ...require("./embeds"),
+
+    // Permissions
+    ...require("./permissions"),
+
+    // Roles
+    ...require("./roles"),
+
+    // Formatting
+    ...require("./formatting"),
+
+    // Validation
+    ...require("./validation"),
+
+    // Logging
+    ...require("./logging"),
+};
